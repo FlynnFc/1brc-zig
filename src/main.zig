0392c6d4ad8aa50cfe6218f5cde2d8e03cb94a65
Flynn <88561679+FlynnFc@users.noreply.github.com>
2025-01-07 20:48:45 +0000

    8 seconds

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const time = std.time;
+const Instant = time.Instant;
+const Timer = time.Timer;
+const print = std.debug.print;
 
 const City = struct {
     min: f64,
@@ -11,17 +15,12 @@ const Error = error{
     InvalidLength,
 };
 
-fn bytesToFloat(bytes: []u8) !f64 {
-    if (bytes.len != 8) {
-        return Error.InvalidLength;
-    }
-
-    const u64Value = @as(u64, bytes.ptr);
-    return @as(f64, u64Value);
-}
-
 pub fn main() !void {
-    var cities = std.StringHashMap(City).init(std.heap.page_allocator);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+    var cities = std.StringHashMap(City).init(allocator);
     defer cities.deinit();
     var file = try std.fs.cwd().openFile("weather_stations.txt", .{});
     defer file.close();
@@ -30,48 +29,41 @@ pub fn main() !void {
     var in_stream = buf_reader.reader();
 
     var buf: [1024]u8 = undefined;
+    const start = try Instant.now();
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var city: []u8 = undefined;
-        var temp: f64 = undefined;
-        for (0.., line) |i, c| {
-            if (c == ';') {
-                temp = try std.fmt.parseFloat(f64, line[i + 1 ..]);
-                city = line[0..i];
-                if (cities.get(city)) |v| {
-                    v.count += 1;
-                    v.sum += temp;
-                    if (v.min == 0 or temp < v.min) {
-                        v.min = temp;
-                    }
-                    if (temp > v.max) {
-                        v.max = temp;
-                    }
-                    break;
-                } else {
-                    const new_city = City{ .min = temp, .max = temp, .sum = temp, .count = 1 };
-                    try cities.put(city, new_city);
-                }
+        var parts = std.mem.splitAny(u8, line, ";");
+        const city: []const u8 = parts.first();
+        const temp: []const u8 = parts.next().?;
+        const temp_f64 = try std.fmt.parseFloat(f64, temp);
+        const city_copy = try allocator.dupe(u8, city);
+        if (cities.getPtr(city_copy)) |v| {
+            v.*.count += 1;
+            v.*.sum += temp_f64;
+            if (v.min == 0 or temp_f64 < v.min) {
+                v.*.min = temp_f64;
             }
+            if (temp_f64 > v.max) {
+                v.*.max = temp_f64;
+            }
+        } else {
+            const new_city = City{ .min = temp_f64, .max = temp_f64, .sum = temp_f64, .count = 1 };
+            try cities.put(city_copy, new_city);
         }
     }
     var iter = cities.iterator();
     while (iter.next()) |entry| {
         const city = entry.key_ptr.*;
         const v = entry.value_ptr.*;
-
-        const avg = v.sum / @as(f64, v.count);
+        const avg = v.sum / @as(f64, @floatFromInt(v.count));
         const min = v.min;
         const max = v.max;
         const count = v.count;
-        const city_str = std.mem.toString(city);
 
-        std.debug.print("City: {}, Min: {}, Max: {}, Avg: {}, Count: {}\n", .{ city_str, min, max, avg, count });
+        std.debug.print("City: {s}, Min: {}, Max: {}, Avg: {}, Count: {}\n", .{ city, min, max, avg, count });
     }
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    const end = try Instant.now();
+    const elapsed1: f64 = @floatFromInt(end.since(start));
+    print("Time elapsed is: {d:.3}ms\n", .{
+        elapsed1 / time.ns_per_ms,
+    });
 }
